b8cc64d522c7a398524040339331efd07566f41a
NikitaRamadhania <ramadhanianikita3@gmail.com>
2026-02-24 11:58:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    /* countdown */
-    const eventDate = new Date("March 8, 2026 00:00:00").getTime();
+        /* countdown */
+        const eventDate = new Date("March 8, 2026 05:00:00").getTime();
 
-    setInterval(() => {
+        setInterval(() => {
 
-        const now = new Date().getTime();
-        const distance = eventDate - now;
+            const now = new Date().getTime();
+            const distance = eventDate - now;
 
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((distance / 1000 / 60) % 60);
-        const seconds = Math.floor((distance / 1000) % 60);
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+            const minutes = Math.floor((distance / 1000 / 60) % 60);
+            const seconds = Math.floor((distance / 1000) % 60);
 
         document.getElementById("days").innerText = days;
         document.getElementById("hours").innerText = hours;
